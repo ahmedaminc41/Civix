eba34ff3a231f8598c3ad566446249cfb0b97f56
ahmed amin <ahmedaminc41@gmail.com>
2024-12-07 01:25:30 +0200

    Reset Password

---
+++ Civix.App.Core/Dtos/Otp/CheckOtpDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Civix.App.Core.Dtos.Otp
+{
+    public class CheckOtpDto
+    {
+        public string Email { get; set; }
+        public string InputOtp { get; set; }
+    }
+}
