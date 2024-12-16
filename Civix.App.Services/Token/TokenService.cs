6219f40a26a101df67739c21b01c6ac7289df83d
Ahmed Shiref <322223713@sha.edu.eg>
2024-12-16 22:42:55 +0200

    Token Service

---
+++ Civix.App.Services/Token/TokenService.cs
@@ -0,0 +1,47 @@
+﻿using Civix.App.Core.Entities;
+using Civix.App.Core.Service.Contracts.Token;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Civix.App.Services.Token
+{
+    public class TokenService : IToken
+    {
+        private readonly IConfiguration _configuration;
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task<string> CreateTokenAsync(AppUser user, UserManager<AppUser> _userManager)
+        {
+            var AuthClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.GivenName,user.Id),
+               
+
+            };
+
+            var UserRole = await _userManager.GetRolesAsync(user);
+            foreach (var Role in UserRole)
+            {
+                AuthClaims.Add(new Claim(ClaimTypes.Role, Role));
+            }
+
+
+            var AuthKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Key"]));
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JWT:ValidIssuer"],
+                audience: _configuration["JWT:ValidAudience"],
+                expires: DateTime.Now.AddDays(double.Parse(_configuration["JWT:DurationInDays"])),
+                claims: AuthClaims,
+                signingCredentials: new SigningCredentials(AuthKey, SecurityAlgorithms.Aes128CbcHmacSha256)
+                );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
