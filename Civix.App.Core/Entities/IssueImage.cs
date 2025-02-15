a5d71de43196b849c6372be80791581dabef118d
ahmed amin <ahmedaminc41@gmail.com>
2025-02-15 23:33:02 +0200

    gfd

---
+++ Civix.App.Core/Entities/IssueImage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Civix.App.Core.Entities
+{
+    public class IssueImage : BaseEntity<int>
+    {
+        public string ImageUrl { get; set; } = string.Empty;
+        public string IssueId { get; set; }
+        public Issue Issue { get; set; }
+    }
+
+}
